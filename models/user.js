a118ff5071181b2908026c2fa7b9d9b5121efabf
abhishekhojha <abhishekhojha45@gmail.com>
2024-04-28 06:13:59 +0530

    06:13 28

--- models/user.js
+++ models/user.js
@@ -1,15 +1,5 @@
 const mongoose = require('mongoose');
-
-const userSchema = new mongoose.Schema({
-  username: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now }
-});
-
-const User = mongoose.model('User', userSchema);
-
-module.exports = User;
+const User = require("../auth/userSchema")
 
 // CRUD operations with validation
 
